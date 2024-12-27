80bfc4b3a628bd05389162b4e4f4b81720ed2b79
MonkeyDluffySan <wwwwwahmedyounis@gmail.com>
2024-12-27 16:42:20 +0200

    fixing-example

--- api-example/main.js
+++ api-example/main.js
@@ -1,14 +1,20 @@
-(function() {
+(function () {
   let xhr = new XMLHttpRequest();
   let url =
     "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
-  onreadystatechange = function() {
+  onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
-      let giphyObj = xhr.responseText;
       let gifDrop = document.querySelector(".gif");
+      let giphyObj = JSON.parse(xhr.responseText);
+      console.log(xhr.responseText);
+
       let link = giphyObj.data[1].images.downsized_medium.url;
       gifDrop.src = link;
+    } else {
+      console.log(JSON.parse(xhr.responseText));
+      console.log(typeof JSON.parse(xhr.responseText));
     }
   };
   xhr.open("GET", url, true);
+  xhr.send();
 })();
